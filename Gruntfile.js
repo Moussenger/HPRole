88f271208efb98b144409deac4ee11e82c3b0672
mou <dhmourelle@gmail.com>
2014-04-01 01:54:12 +0200

    Added Grunt less and Bootstrap

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,10 +12,27 @@ module.exports = function(grunt) {
                 files: ['src/**/*.js', 'test/unit/**/*.js'],
                 tasks: ['karma:unit:run']
             }
+        },
+        less: {
+            bootstrap: {
+                options: {
+                    paths:["frontend/dev/less/bootstrap/"],
+                    cleancss: true,
+                    strictMath: true,
+                    sourceMap: true,
+                    outputSourceFiles: true,
+                    sourceMapURL: 'bootstrap.css.map',
+                    sourceMapFilename: 'frontend/public/css/bootstrap.css.map'
+                },
+                files: {
+                    "frontend/public/css/bootstrap.css": "**/bootstrap.less"
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-karma');
 
     grunt.registerTask('devmode', ['karma:unit', 'watch']);
