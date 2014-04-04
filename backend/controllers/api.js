a8948607f790937556ef465176996179f3860aaf
mou <dhmourelle@gmail.com>
2014-04-04 02:50:41 +0200

    Add initial api

---
+++ backend/controllers/api.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose"),
+    Spell    = mongoose.model("Spell"); 
+    api      = {};
+
+
+api.findSpellByName = function (req, res) {
+    var name = req.param("name");
+
+    Spell.findByName(name, function (err, spell) {
+        if(!err) {
+            res.set({
+                'Content-Type': 'application/json; charset=utf-8'
+            });
+            
+            res.send(spell);
+        } else {
+            res.send(404);
+        }
+    });
+}
+
+
+exports = api;
